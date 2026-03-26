7bd9aaf4f25ff850b30d7d1a825e545c38f981c4
yashika.bajaj@smartbear.com <yashika.bajaj@smartbear.com>
2026-03-26 12:34:06 +0530

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20780.stoplight-dev.com/api/v1/projects/cHJqOjI1MjE/spectral.js?branch_slug=main&token=7e1f6349-476b-452b-b2b9-fcbe93037c79";
+export default { extends: ruleset };
---
+++ docs/1w2qsdefrgth.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: li07ezaos5e9y
+---
+
+# 1w2qsdefrgth
+
+The beginning of an awesome article...
